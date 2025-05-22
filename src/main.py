71b884d6ec92e46b18d33486d438945859049686
Ryan Agdassi <113940065+ragdassi@users.noreply.github.com>
2025-05-21 17:01:43 -0700

    update

--- src/main.py
+++ src/main.py
@@ -11,10 +11,10 @@ brunson_dataset = PlayerDataset(BRUNSON_ID)
 evaluator = ParlayEvaluator()
 
 #print(brunson_dataset.get_season(2024))
-#evaluator.train_player_prop_model(BRUNSON_ID, "pts", brunson_dataset.get_season(2024))
+evaluator.train_player_prop_model(BRUNSON_ID, "pts", brunson_dataset.get_season(2024))
 
 # OLD
 # predictor = TabularPredictor.load("/Users/star/Library/CSC/CSC480/NBA-Analytics-AI/AutogluonModels/ag-20250516_231857")
-predictor = TabularPredictor.load("/Users/star/Library/CSC/CSC480/NBA-Analytics-AI/AutogluonModels/ag-20250516_234048")
-print("leaderboard", predictor.leaderboard())
-print(predictor.predict(brunson_dataset.get_playoffs(2024)))
+# predictor = TabularPredictor.load("/Users/star/Library/CSC/CSC480/NBA-Analytics-AI/AutogluonModels/ag-20250516_234048")
+# print("leaderboard", predictor.leaderboard())
+# print(predictor.predict(brunson_dataset.get_playoffs(2024)))
