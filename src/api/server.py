d748e490ce19a39059b1bc527f25f3731fd9cbd9
jangomax <jangomaxstudio@gmail.com>
2025-05-30 19:36:00 -0700

    Api work

--- src/api/server.py
+++ src/api/server.py
@@ -1,38 +1,31 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.responses import HTMLResponse
 from api.model import *
+from model.parlay_evaluator import ParlayEvaluator
 
-router = APIRouter(prefix="/api")
+class parlaiApi:
+    def __init__(self, evaluator: ParlayEvaluator):
+        self.evaluator = evaluator
+        self.router = APIRouter(prefix="/api")
+        self._setup_routes()
+    
+    def _setup_routes(self):
+        @self.router.post("/parlay/", response_model=PostParlayResponse)
+        def post_parlay(request: PostParlayRequest):
+          parlay_leg_responses: List[ParlayLegProbability] = []
+          overall_probability = 1
+          for parlayLeg in request.parlayLegs:
+            evaluated = self.evaluator.evaluate_leg(parlayLeg)
 
-@router.get("/")
-def root():
-  html_content = """
-    <html>
-        <head>
-            <title>ParlAI</title>
-        </head>
-        <body>
-            <h1>ParlAI</h1>
-        </body>
-    </html>
-    """
-  return HTMLResponse(content=html_content, status_code=200)
+            parlay_leg_responses.append(evaluated)
+            overall_probability *= evaluated.probability
 
-@router.post("/parlay/", response_model=PostParlayResponse)
-def post_parlay(request: PostParlayRequest):
-  return PostParlayResponse(**{
-    "parlayLegProbabilities": [
-        {
-            "player": "Stephen Curry",
-            "opponent": "HOU",
-            "prop": "pts",
-            "value": 25.5,
-            "overUnder": "over",
-            "probability": 0.42
-        }
-    ],
-    "overallProbability": 0.42
-})
+          return PostParlayResponse(
+            parlayLegProbabilities=parlay_leg_responses,
+            overallProbability=overall_probability
+          )
 
-app = FastAPI()
-app.include_router(router)
+    def start_server(self):
+        app = FastAPI()
+        app.include_router(self.router)
+        return app
--- src/model/parlay_evaluator.py
+++ src/model/parlay_evaluator.py
@@ -2,12 +2,24 @@ from typing import Dict, Tuple, List
 from autogluon.tabular import TabularPredictor
 import pandas as pd
 
-from model.model_registry import ModelRegistry
-from api.model import *
+from model.quantile_converter import QuantileConverter
+
+from data.player_dataset import PlayerDataset
+from data.download import generate_player_id
+
+from api.model import players as player_list
+from api.model import ParlayLeg
+from api.model import ParlayLegProbability
+from api.model import PropStat
+
+QUANTILE_LEVELS = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
 class ParlayEvaluator:
     def __init__(self):
-        self.model_registry = ModelRegistry()
+        self.quantile_converter = QuantileConverter(quantile_levels=QUANTILE_LEVELS)
+
+        player_ids = [generate_player_id(player) for player in player_list]
+        self.player_datasets = {player_id: PlayerDataset(player_id) for player_id in player_ids}
         
     def train_player_prop_model(self, player_id: str, prop: PropStat, historical_data: pd.DataFrame) -> None:
         """
@@ -26,7 +38,8 @@ class ParlayEvaluator:
             path=f"model/{player_id}/{prop}/",
             label=label,
             problem_type='quantile', 
-            quantile_levels=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+            quantile_levels=QUANTILE_LEVELS,
+            sample_weight="is_playoff"
         )
         
         # Train model
@@ -35,11 +48,20 @@ class ParlayEvaluator:
             presets='best_quality',
             time_limit=300 # 5 min
         )
-        
-        # Save model
-        self.model_registry.save_model(predictor, player_id, prop)
 
-    def predict(self, player_id: str, prop: PropStat, row: pd.DataFrame):
+    def predict(self, player_id: str, prop: PropStat, row: pd.DataFrame) -> List[float]:
         predictor = TabularPredictor.load(f"./model/{player_id}/{prop}")
 
-        return predictor.predict(row)
+        return predictor.predict(row).iloc[0].values.tolist()
+
+    def evaluate_leg(self, parlay_leg: ParlayLeg):
+        player_id = generate_player_id(parlay_leg.player)
+        
+        prediction_row = self.player_datasets[player_id].get_playoffs_avg(10)
+        quantile_values = self.predict(player_id, parlay_leg.prop.lower(), prediction_row)
+        result = self.quantile_converter.quantiles_to_hit_probability(quantile_values, float(parlay_leg.value))
+
+        return ParlayLegProbability(
+            **parlay_leg.model_dump(),
+            probability=result[parlay_leg.overUnder]
+        )
