c44ad8d720017b465356f40423d8b198065893d8
jangomax <jangomaxstudio@gmail.com>
2025-05-31 18:39:51 -0700

    Attempt reduce overfit

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ from model.parlay_evaluator import ParlayEvaluator
 from data.player_dataset import PlayerDataset
 from api.model import PropStat
 from data.download import get_player_ids_from_rosters
+import os
 
 evaluator = ParlayEvaluator()
 
@@ -9,6 +10,9 @@ props = PropStat._member_names_ # get list of all prop values from our enum in a
 
 def train_player_props(player_id: str):
     for prop in props:
+        if os.path.exists(f"model/{player_id}/{prop}/"):
+            print(f"Model for {player_id}/{prop} already exists")
+            continue
         player_data = PlayerDataset(player_id)
         evaluator.train_player_prop_model(player_id, prop, player_data.get_stats())
 
@@ -17,3 +21,5 @@ def train_all():
 
     for player_id in player_ids:
         train_player_props(player_id)
+
+train_player_props("halibty01")
